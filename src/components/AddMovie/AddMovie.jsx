6d3ce319d2fd1811587f018a312947bbdeac6d6b
Riley DuBord <thelifeofdu@gmail.com>
2024-02-10 11:49:05 -0600

    react bootstrap styles applied to AddMovie form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,56 +1,94 @@
 import { useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Button } from "react-bootstrap";
 
-export default function AddMovie(){
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const [movieTitle, setMovieTitle] = useState('')
-    const [moviePoster, setMoviePoster] = useState('')
-    const [movieDescription, setMovieDescription] = useState('')
+export default function AddMovie() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [movieTitle, setMovieTitle] = useState("");
+  const [moviePoster, setMoviePoster] = useState("");
+  const [movieDescription, setMovieDescription] = useState("");
 
-    const handleInputChangeTitle = (e) => {
-        setMovieTitle(e.target.value);
-      };
+  const handleInputChangeTitle = (e) => {
+    setMovieTitle(e.target.value);
+  };
 
-      const handleInputChangePoster = (e) => {
-        setMoviePoster(e.target.value);
-      };
+  const handleInputChangePoster = (e) => {
+    setMoviePoster(e.target.value);
+  };
 
-      const handleInputChangeDesc = (e) => {
-        setMovieDescription(e.target.value);
-      };
+  const handleInputChangeDesc = (e) => {
+    setMovieDescription(e.target.value);
+  };
 
-    const movieData = {
-        title: movieTitle,
-        poster: moviePoster,
-        description: movieDescription,
-        genre_id: 2
-    }
+  const movieData = {
+    title: movieTitle,
+    poster: moviePoster,
+    description: movieDescription,
+    genre_id: 2,
+  };
 
-    function postMovie() {
-        console.log('checking payload of submit', movieData);
-        dispatch({
-            type: 'POST_MOVIE',
-            payload: movieData
-        })
-        setMovieTitle('')
-        setMoviePoster('')
-        setMovieDescription('')
-    }
+  function postMovie() {
+    console.log("checking payload of submit", movieData);
+    dispatch({
+      type: "POST_MOVIE",
+      payload: movieData,
+    });
+    setMovieTitle("");
+    setMoviePoster("");
+    setMovieDescription("");
+  }
 
-    return (
-        <>
-        <h1>IN ADD MOVIE </h1>
-        <form action="">
-            <input type="text" name="" placeholder="Movie Title" id="" value={movieTitle} onChange={handleInputChangeTitle}/>
-            <input type="text" name="" placeholder="Movie Poster URL" id="" value={moviePoster} onChange={handleInputChangePoster}/>
-            <input type="text" name="" placeholder="Movie Description" id="" value={movieDescription} onChange={handleInputChangeDesc}/>
-            <input type="text" name="" placeholder="DROPDOWN PLACEHOLDER" id="" />
-            {/* use genre reducer for dropdown */}
-        </form>
-        <button  onClick={() => history.push(`/`)}>GO BACK</button>
-        <button  onClick={() => {history.push(`/`); postMovie();}}>ADD MOVIE</button> 
-        </>
-    )
+  return (
+    <>
+      <h1>IN ADD MOVIE </h1>
+      <form action="">
+        <div class="form-group">
+          <div class="form-floating mb-3">
+            <input
+              type="text"
+              class="form-control"
+              id="floatingInput"
+              value={movieTitle}
+              onChange={handleInputChangeTitle}
+            />
+            <label for="floatingInput">Enter Movie Title Here</label>
+          </div>
+
+          <div class="form-floating mb-3">
+            <input
+              type="url"
+              class="form-control"
+              id="floatingInput"
+              value={moviePoster}
+              onChange={handleInputChangePoster}
+            />
+            <label for="floatingInput">Enter Movie Poster URL Here</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              type="text"
+              class="form-control"
+              id="floatingInput"
+              value={movieDescription}
+              onChange={handleInputChangeDesc}
+            />
+            <label for="floatingInput">Enter Movie Description</label>
+          </div>
+          <input type="text" name="" placeholder="DROPDOWN PLACEHOLDER" id="" />
+          {/* use genre reducer for dropdown */}
+        </div>
+      </form>
+      <Button onClick={() => history.push(`/`)}>GO BACK</Button>
+      <Button
+        onClick={() => {
+          history.push(`/`);
+          postMovie();
+        }}
+      >
+        ADD MOVIE
+      </Button>
+    </>
+  );
 }
